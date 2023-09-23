1d70320b6f8e4f19a00d8409f698ce990039b607
Zigguraptor <cobaltum8@gmail.com>
2023-09-23 13:16:22 +0400

    added SdecApiService.cs

---
+++ ShippingCalculator.WebApi/SdecApiService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Net.Http.Headers;
+using ShippingCalculator.WebApi.Configurations;
+
+namespace ShippingCalculator.WebApi;
+
+public class SdecApiService
+{
+    private readonly HttpClient _httpClient;
+    private readonly SdecApiConfiguration _sdecApiConfiguration;
+
+    public SdecApiService(HttpClient httpClient, SdecApiConfiguration sdecApiConfiguration)
+    {
+        _httpClient = httpClient;
+        _sdecApiConfiguration = sdecApiConfiguration;
+
+        _httpClient.DefaultRequestHeaders.Add(HeaderNames.Authorization, sdecApiConfiguration.Token);
+        _httpClient.BaseAddress = _sdecApiConfiguration.BaseUri;
+    }
+}
