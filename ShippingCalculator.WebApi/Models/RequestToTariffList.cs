9d2e6cf4f9a0eb2f506bf35017daf4d4c6211ccf
Zigguraptor <cobaltum8@gmail.com>
2023-09-23 13:11:47 +0400

    added RequestToTariffList.cs

---
+++ ShippingCalculator.WebApi/Models/RequestToTariffList.cs
@@ -0,0 +1,10 @@
+﻿namespace ShippingCalculator.WebApi.Models;
+
+public class RequestToTariffList
+{
+    public int type { get; set; }
+    public int currency { get; set; }
+    public Location from_location { get; set; }
+    public Location to_location { get; set; }
+    public IEnumerable<Package> packages { get; set; }
+}
