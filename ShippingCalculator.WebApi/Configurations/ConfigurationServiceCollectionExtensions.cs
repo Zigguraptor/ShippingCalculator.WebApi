1a523c9f0e7e5722f86c07cabd74c85bcbb37e14
Zigguraptor <cobaltum8@gmail.com>
2023-09-23 11:50:28 +0400

    added ConfigurationServiceCollectionExtensions

---
+++ ShippingCalculator.WebApi/Configurations/ConfigurationServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace ShippingCalculator.WebApi.Configurations;
+
+public static class ConfigurationServiceCollectionExtensions
+{
+    public static IServiceCollection AddConfigurations(this IServiceCollection services)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+        var sdecApiConfiguration = new SdecApiConfiguration();
+
+        configuration.GetSection("CdekApi").Bind(sdecApiConfiguration);
+
+        services.AddSingleton(sdecApiConfiguration);
+
+        return services;
+    }
+}
