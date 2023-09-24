3c9c625b90575370b1c26215376c6261bca375fb
Zigguraptor <cobaltum8@gmail.com>
2023-09-24 17:09:59 +0400

    made fields nullable

--- ShippingCalculator.WebApi/Models/RequestToTariffList.cs
+++ ShippingCalculator.WebApi/Models/RequestToTariffList.cs
@@ -6,7 +6,7 @@ public class RequestToTariffList
 {
     [JsonPropertyName("type")] public int Type { get; set; }
     [JsonPropertyName("currency")] public int Currency { get; set; }
-    [JsonPropertyName("from_location")] public Location FromLocation { get; set; }
-    [JsonPropertyName("to_location")] public Location ToLocation { get; set; }
-    [JsonPropertyName("packages")] public IEnumerable<Package> Packages { get; set; }
+    [JsonPropertyName("from_location")] public Location? FromLocation { get; set; }
+    [JsonPropertyName("to_location")] public Location? ToLocation { get; set; }
+    [JsonPropertyName("packages")] public IEnumerable<Package>? Packages { get; set; }
 }
