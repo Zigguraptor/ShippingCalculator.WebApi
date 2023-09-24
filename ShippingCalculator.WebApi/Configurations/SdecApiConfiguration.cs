fd4dad8cd0a788ef4f98124024e9850832c47c39
Zigguraptor <cobaltum8@gmail.com>
2023-09-24 16:55:17 +0400

    deleted token

--- ShippingCalculator.WebApi/Configurations/SdecApiConfiguration.cs
+++ ShippingCalculator.WebApi/Configurations/SdecApiConfiguration.cs
@@ -6,7 +6,6 @@ public class SdecApiConfiguration
     public string AuthorizationUri { get; set; } = null!;
     public string ClientId { get; set; } = null!;
     public string ClientSecret { get; set; } = null!;
-    public string Token { get; set; } = null!;
     public string LocationCitiesUrl { get; set; } = null!;
     public string FiasGuidParameter { get; set; } = null!;
     public string CalculatorTarifflistUrl { get; set; } = null!;
