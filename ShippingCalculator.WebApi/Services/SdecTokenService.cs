2dadd33226225a8ceae87e7574f7ccfe384f2560
Zigguraptor <cobaltum8@gmail.com>
2023-09-24 16:29:52 +0400

    added SdecTokenService.cs

---
+++ ShippingCalculator.WebApi/Services/SdecTokenService.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+using ShippingCalculator.WebApi.Configurations;
+using ShippingCalculator.WebApi.Models;
+
+namespace ShippingCalculator.WebApi.Services;
+
+public class SdecTokenService
+{
+    private readonly HttpClient _httpClient;
+    private readonly SdecApiConfiguration _sdecApiConfiguration;
+    private string _token;
+    private DateTime _expiringTime;
+
+    public SdecTokenService(HttpClient httpClient, SdecApiConfiguration sdecApiConfiguration)
+    {
+        _httpClient = httpClient;
+        _sdecApiConfiguration = sdecApiConfiguration;
+        _httpClient.BaseAddress = _sdecApiConfiguration.BaseUri;
+        _token = GetNewToken();
+    }
+
+    private string GetNewToken()
+    {
+        var parameters =
+            $"?grant_type=client_credentials&client_id={_sdecApiConfiguration.ClientId}&client_secret={_sdecApiConfiguration.ClientSecret}";
+        using var httpResponseMessage = _httpClient
+            .PostAsync(_sdecApiConfiguration.AuthorizationUri + parameters, new StringContent("")).Result;
+
+        if (!httpResponseMessage.IsSuccessStatusCode)
+            throw new Exception($"SDEC authorization error: {httpResponseMessage.ReasonPhrase}");
+
+        var responseMessage = httpResponseMessage.Content.ReadAsStringAsync().Result;
+        var accessToken = JsonSerializer.Deserialize<AuthorizationResponse>(responseMessage);
+
+        if (accessToken?.AccessToken is null) throw new JsonException(responseMessage);
+
+        _expiringTime = DateTime.Now + TimeSpan.FromMilliseconds((int)(accessToken.ExpiresIn * 0.9d));
+
+        return accessToken.AccessToken;
+    }
+}
