2c3a3767f7a20d0b2eddb72ed11b54be666a3f44
Zigguraptor <cobaltum8@gmail.com>
2023-09-24 10:35:01 +0400

    added authorization data

--- ShippingCalculator.WebApi/Configurations/SdecApiConfiguration.cs
+++ ShippingCalculator.WebApi/Configurations/SdecApiConfiguration.cs
@@ -3,6 +3,9 @@
 public class SdecApiConfiguration
 {
     public Uri BaseUri { get; set; } = null!;
+    public string AuthorizationUri { get; set; } = null!;
+    public string ClientId { get; set; } = null!;
+    public string ClientSecret { get; set; } = null!;
     public string Token { get; set; } = null!;
     public string LocationCitiesUrl { get; set; } = null!;
     public string FiasGuidParameter { get; set; } = null!;
