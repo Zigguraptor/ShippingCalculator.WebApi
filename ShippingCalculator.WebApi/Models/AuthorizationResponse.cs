9c79fce7ef6d4334348f927c1436e574b98d0b02
Zigguraptor <cobaltum8@gmail.com>
2023-09-24 11:03:05 +0400

    added AuthorizationResponse.cs

---
+++ ShippingCalculator.WebApi/Models/AuthorizationResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ShippingCalculator.WebApi.Models;
+
+public class AuthorizationResponse
+{
+    [JsonPropertyName("access_token")] public string? AccessToken { get; set; }
+    [JsonPropertyName("expires_in")] public int ExpiresIn { get; set; }
+}
